665e04aae199ecc0f8d1c96ef6abd0ed5450497a
denismaster <denismaster@outlook.com>
2018-08-18 17:25:31 +0300

    Add SettingsTable class skeleton

---
+++ src/SettingsTable/SettingsTable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SettingsTable
+{
+    public class SettingsTable<TKey, TValue> : IClearable where TValue : class
+    {
+        public List<SettingsTableVersion<TKey, TValue>> Versions { get; private set; } = new List<SettingsTableVersion<TKey, TValue>>();
+
+        public void AddVersion(SettingsTableVersion<TKey, TValue> version)
+        {
+            Versions.Add(version);
+        }
+
+        public void Clear()
+        {
+            Versions.Clear();
+        }
+    }
+}
---
+++ tests/SettingsTable.Tests/SettingsTableTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SettingsTable.Tests
+{
+    public class SettingsTableTests
+    {
+        #region AddVersion tests
+
+        [Fact]
+        public void AddVersionShouldAddToVersionList()
+        {
+            var table = new SettingsTable<string,string>();
+
+            Assert.NotNull(table.Versions);
+            Assert.Empty(table.Versions);
+
+            var version = new SettingsTableVersion<string, string>();
+
+            table.AddVersion(version);
+
+            Assert.NotEmpty(table.Versions);
+            Assert.Collection(table.Versions, v => Assert.Equal(v, version));
+        }
+
+        #endregion
+    }
+}
