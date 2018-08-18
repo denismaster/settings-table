cb9b2ff2562de592269a187bff05c1026dc6b37f
denismaster <denismaster@outlook.com>
2018-08-18 18:36:56 +0300

    Add IEquatable implementation for SettingsTable

--- src/SettingsTable/SettingsTable.cs
+++ src/SettingsTable/SettingsTable.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SettingsTable
 {
-    public class SettingsTable<TKey, TValue> : IClearable where TValue : class
+    public class SettingsTable<TKey, TValue> : IClearable, IEquatable<SettingsTable<TKey,TValue>> 
+        where TValue : class
     {
         public List<SettingsTableVersion<TKey, TValue>> Versions { get; } = new List<SettingsTableVersion<TKey, TValue>>();
 
+        /// <summary>
+        /// Add new version to the table
+        /// </summary>
+        /// <param name="version">A new SettingsTableVersion, which should be added to the table</param>
         public void AddVersion(SettingsTableVersion<TKey, TValue> version)
         {
             if (version == null)
@@ -21,5 +27,36 @@ namespace SettingsTable
         /// Remove all versions from the table
         /// </summary>
         public void Clear() => Versions.Clear();
+
+        public bool Equals(SettingsTable<TKey, TValue> other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other)) return true;
+
+            return Versions.SequenceEqual(other.Versions);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj)) return true;
+
+            if (obj.GetType() != this.GetType()) return false;
+
+            return Equals((SettingsTable<TKey, TValue>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Versions.GetHashCode();
+        }
     }
 }
--- tests/SettingsTable.Tests/SettingsTableTests.cs
+++ tests/SettingsTable.Tests/SettingsTableTests.cs
@@ -55,5 +55,78 @@ namespace SettingsTable.Tests
         }
 
         #endregion
+
+        #region EqualsTests
+
+        [Fact]
+        public void EqualsShouldReturnFalseWhenComparingToNull()
+        {
+            var table = new SettingsTable<string, string>();
+
+            Assert.False(table.Equals(null));
+        }
+
+        [Fact]
+        public void EqualsShouldReturnFalseWhenTypesAreNotEqual()
+        {
+            var table = new SettingsTable<string, string>();
+
+            Assert.False(table.Equals(new object()));
+        }
+
+        [Fact]
+        public void EqualsShouldReturnTrueWhenReferencesAreEqual()
+        {
+            var table = new SettingsTable<string, string>();
+            var otherTable = table;
+
+            Assert.True(table.Equals(otherTable));
+        }
+
+        [Fact]
+        public void EqualsShouldReturnTrueWhenAllVersionsAreEqualInTwoTables()
+        {
+            var version = new SettingsTableVersion<string,string>();
+
+            var table = new SettingsTable<string, string>();
+            var otherTable = new SettingsTable<string, string>();
+
+            table.AddVersion(version);
+            otherTable.AddVersion(version);
+
+            Assert.True(table.Equals(otherTable));
+        }
+
+        [Fact]
+        public void EqualsShouldReturnFalseWhenAllVersionsAreEqualInTwoTables()
+        {
+            var version = new SettingsTableVersion<string, string>();
+            var anotherVersion = new SettingsTableVersion<string, string>();
+
+            var table = new SettingsTable<string, string>();
+            var otherTable = new SettingsTable<string, string>();
+
+            table.AddVersion(version);
+            otherTable.AddVersion(anotherVersion);
+
+            Assert.False(table.Equals(otherTable));
+        }
+
+        #endregion
+
+        #region GetHashCodeTests
+
+        [Fact]
+        public void GetHashCodeShouldReturnHashCodeOfAllVersions()
+        {
+            var table = new SettingsTable<string, string>();
+            table.AddVersion(new SettingsTableVersion<string, string>());
+
+            int versionsHashCode = table.Versions.GetHashCode();
+
+            Assert.Equal(versionsHashCode, table.GetHashCode());
+        }
+
+        #endregion
     }
 }
